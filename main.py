355c09414a4da75dc083d5287913adf6e7484529
rootbeeraugury <155499732+rootbeeraugury@users.noreply.github.com>
2024-01-31 08:01:33 +0000

    forms classwork + some list/view classwork from today

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+
+#create a secret key for security
+import os 
+import json
 
 import utils as util
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY', 'default_secret_key')
 
 @app.route('/')
 def index():
@@ -41,5 +47,53 @@ def movies():
       "movies": movie_dict
     }
     return render_template("movies.html", **context)
+
+# Route for the form page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    title = "formsapalooza"
+    feedback = None
+    if request.method == 'POST':
+        feedback = register_data(request.form)
+
+    context = {
+        "title": title,
+        "feedback": feedback
+    }
+    return render_template('register.html', **context)
+
+
+def register_data(form_data):
+  feedback = []
+  for key, value in form_data.items():
+  # checkboxes have [] for special handling
+    if key.endswith('[]'):
+    # Use getlist to get all values for the checkbox
+      checkbox = request.form.getlist(key)
+      key = key.replace('_', ' ').replace('[]', '')
+      feedback.append(f"{key}: {', '.join(map(str, checkbox))}")
+    else:
+    # Handle other form elements
+      key = key.replace('_', ' ')
+      feedback.append(f"{key}: {value}")
+  return feedback
+
+@app.route('/users')
+def users():
+  # Read project data from JSON file
+  with open('test.json') as json_file:
+    user_data = json.load(json_file)
+    print(user_data)
+
+    context = {
+        "title": "users list",
+        "users": user_data
+    }
+    return render_template("users.html", **context)
+
+@app.route('/user/<int:user_number>')
+def user(user_number):
+    return f'Content for User {user_number}:'
+
 
 app.run(host='0.0.0.0', port=81)
