fbaadc77f9d8d84f5acf70b5ecc93bda9015226f
[redacted] <155499732+rootbeeraugury@users.noreply.github.com>
2024-03-13 00:24:01 +0000

    all done with flask work for now

--- main.py
+++ main.py
@@ -1,25 +1,201 @@
-from flask import Flask, render_template, request, url_for
-
-#create a secret key for security
-import os 
 import json
+
+import os
+
+from flask import Flask, flash, redirect, render_template, request, url_for, redirect, flash
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_login import (
+  LoginManager,
+  login_user,
+  logout_user,
+)
+#add CSRF protection to forms
+from flask_wtf import CSRFProtect
+
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash
+from models import db, Recipe, Category, Chef
+
+#we may not need this as we're not using it directly
+from email_validator import validate_email, EmailNotValidError
+
+#wtf forms import
+from forms import RecipeAdd, RecipeEdit, LoginForm, RegistrationForm, RecipePicForm
 
 import utils as util
 
-from models import db
-from models.category import Category
-from models.recipe import Recipe
-
 # loads default recipe data
-from default_data import create_default_data 
+from default_data import create_default_data
+from models import Category, Chef, Recipe, db, Chef
 
 app = Flask(__name__)
 
+# Set the upload folder for recipe pictures
+UPLOAD_FOLDER = 'static/recipe_pics'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#create a secret key for security
 app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY', 'default_secret_key')
+
+#add csrf after secret key
+csrf = CSRFProtect(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
 db.init_app(app)
 
+#setup login
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+#LOGIN MANAGER
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(Chef, int(user_id))
+
+#LOGIN
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    title = "Chez Chef"
+    # Override next on query string to display warning
+    next_url = request.args.get('next')
+    if next_url:
+        flash('Please log in to access this page.', 'warning')
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        chef = Chef.query.filter_by(email=form.email.data).first()
+        if chef and chef.check_password(form.password.data):
+            login_user(chef)
+            next_page = request.args.get('next')
+            flash('Login Successful!', 'success')
+            return redirect(next_page or url_for('index'))
+        else:
+            flash('Login or password incorrect', 'error')
+
+    #form did NOT validate
+    if request.method == 'POST' and not form.validate():
+          for field, errors in form.errors.items():
+              for error in errors:
+                  flash(f"Error in {field}: {error}", 'error')
+    context = {
+        "title": title,
+        "form": form
+    }
+    return render_template('login.html',**context)
+
+#LOGOUT
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('Logout Successful!', 'success')
+    return redirect(url_for('index'))
+
+#SIGN_UP
+@app.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+    title = "Chez Chef"
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        chef= Chef(first_name=form.first_name.data,
+                     last_name=form.last_name.data,
+                     email=form.email.data)
+        chef.set_password(form.password.data)
+        db.session.add(chef)
+        db.session.commit()
+        flash('Registration successful! Please log in.', 'success')
+        return redirect(url_for('login'))
+    #form did NOT validate
+    if request.method == 'POST' and not form.validate():
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash(f"Error in {field}: {error}", 'error')
+
+    context = {
+        "title": title,
+        "form": form
+    }
+    return render_template('sign_up.html', **context)
+###
+
+###
+@app.route('/add_recipe', methods=['GET', 'POST'])
+@login_required
+def add_recipe():
+      form = RecipeAdd()
+
+      # Populate the category choices dynamically
+      form.category_id.choices = [(category.id, category.name) for category in Category.query.all()]
+
+      if request.method == 'POST' and form.validate_on_submit():
+          # Create a new recipe instance and add it to the database
+        new_recipe = Recipe(
+          name=form.name.data,
+          author=form.author.data,
+          description=form.description.data,
+          ingredients=form.ingredients.data,
+          instructions=form.instructions.data,
+          rating=form.rating.data,
+          category_id=form.category_id.data
+          )
+        db.session.add(new_recipe)
+        db.session.commit()
+
+          #inform user of success!
+        flash('Recipe added successfully!', 'success')
+        return redirect(url_for('recipes'))
+
+      #form did NOT validate
+      if request.method == 'POST' and not form.validate():
+            for field, errors in form.errors.items():
+                for error in errors:
+                    flash(f"Error in {field}: {error}", 'error')
+            return render_template('add_recipe.html', form=form)
+
+      #default via GET shows form  
+      return render_template('add_recipe.html', form=form)
+
+
+#EDIT RECIPE
+@app.route('/edit_recipe/<int:recipe_id>', methods=['GET', 'POST'])
+@login_required
+
+def edit_recipe(recipe_id):
+    # Retrieve the recipe from the database
+    recipe = Recipe.query.get_or_404(recipe_id)
+    form = RecipeEdit(obj=recipe)
+
+    # Populate categories in the form
+    form.category_id.choices = [(category.id, category.name) for category in Category.query.all()]
+
+
+    if request.method == 'POST' and form.validate_on_submit():
+        form.populate_obj(recipe)  # Update the recipe object with form data
+        db.session.commit()
+        flash('Recipe updated successfully!', 'success')
+        return redirect(url_for('recipes'))
+
+    #form did NOT validate
+    if request.method == 'POST' and not form.validate():
+          for field, errors in form.errors.items():
+              for error in errors:
+                  flash(f"Error in {field}: {error}", 'error')
+          return render_template('edit_recipe.html', form=form, recipe=recipe)
+
+    return render_template('edit_recipe.html', form=form, recipe=recipe)
+
+  #DELETE
+@app.route('/delete_recipe/<int:id>', methods=['POST'])
+@login_required
+def delete_recipe(id):
+      recipe = Recipe.query.get_or_404(id)
+      db.session.delete(recipe)
+      db.session.commit()
+      flash('Recipe deleted successfully!', 'success')
+      return redirect(url_for('recipes'))
+
+###
 
 @app.route('/')
 def index():
@@ -134,8 +310,7 @@ def recipes():
 
 @app.route("/recipe/<int:recipe_id>")
 def recipe(recipe_id):
-    this_recipe = Recipe.query.get(recipe_id)
-    title = "Recipe"
+    this_recipe = db.session.get(Recipe, recipe_id)
     context = {
       "title": "Recipe",
       "recipe": this_recipe
@@ -145,6 +320,29 @@ def recipe(recipe_id):
     else:
         return render_template("404.html",title="404"), 404
 
+###
+#RECIPE_PIC
+@app.route('/recipe_pic/<int:recipe_id>', methods=['GET', 'POST'])
+def recipe_pic(recipe_id):
+    form = RecipePicForm()  # Instantiate the form
+    if form.validate_on_submit():
+        # Save the uploaded file
+        file = form.picture.data
+        filename = f"recipe_{recipe_id}.jpg"
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('recipe', recipe_id=recipe_id))
+    return render_template('recipe_pic.html', form=form)
+###
+
+#flask-admin!
+class RecipeView(ModelView):
+  column_searchable_list = ['name', 'author']
+
+admin = Admin(app)
+admin.url = '/admin/' #would not work on repl w/o this!
+admin.add_view(RecipeView(Recipe, db.session))
+admin.add_view(ModelView(Category, db.session))
+admin.add_view(ModelView(Chef, db.session))
 
 with app.app_context():
   db.create_all()
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ def movie_stars(movie_dict):
     movie['stars'] = add_stars(movie['rating'])
   return movie_dict
 
+
 def add_stars(rating):
   my_return = ""
   for x in range(5):
@@ -10,24 +11,26 @@ def add_stars(rating):
     my_return += f"<span class=\"fa fa-star {checked}\"></span>"
   return my_return
 
+
 def register_data(form_data):
   feedback = []
   for key, value in form_data.items():
-# checkboxes have [] for special handling
+    # checkboxes have [] for special handling
     if key.endswith('[]'):
-  # Use getlist to get all values for the checkbox
+      # Use getlist to get all values for the checkbox
       checkbox = request.form.getlist(key)
       key = key.replace('_', ' ').replace('[]', '')
       feedback.append(f"{key}: {', '.join(map(str, checkbox))}")
     else:
-  # Handle other form elements
+      # Handle other form elements
       key = key.replace('_', ' ')
       feedback.append(f"{key}: {value}")
   return feedback
 
+
 def load_user_data(user_number):
   # Read project data from JSON file
   with open('test.json') as json_file:
-      user_data = json.load(json_file)
-      user = next((u for u in user_data if u['id'] == user_number), None)
-      return user
+    user_data = json.load(json_file)
+    user = next((u for u in user_data if u['id'] == user_number), None)
+    return user
